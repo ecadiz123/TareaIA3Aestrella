c5e790039842006e55d09a31056d908fe772d74f
Esteban <estebancadiz@gmail.com>
2024-10-15 09:34:33 -0300

    borrado metodo de heuristica q no sirve

--- Aestrella/Aestrella/Laberinto.cs
+++ Aestrella/Aestrella/Laberinto.cs
@@ -12,11 +12,5 @@ namespace Aestrella
          * no tienes obstaculos. Si heuristica es mayor a costo real, no es admisible.
          * Siempre debe ser menor igual a costo
          * */
-        public int hManhattan( int x1,int y1,int x2,int y2)// Funcion que va a calcular heuristica de manhattan dado dos puntos, inicial y final (x1,y1) inicial-(x2,y2) final
-        {
-            int xval=Math.Abs(x1 - x2);
-            int yval=Math.Abs(y1 - y2);
-            return xval + yval;
-        }
     }
 }
