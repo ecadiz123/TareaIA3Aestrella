4fe2a04c32504d2bc88b3489c6a6718fedac8a7d
ecadiz123 <estebancadiz@gmail.com>
2024-10-21 17:01:09 -0300

    definida la clase Nodo para trabajar con algoritmo

--- Aestrella/Aestrella/Laberinto.cs
+++ Aestrella/Aestrella/Laberinto.cs
@@ -9,16 +9,10 @@ using System.ComponentModel;
 
 namespace Aestrella
 {
-    class Par//Clase simple que guarda un par, uno es el pto el otro es la heuristica. Usado solo en el metodo para encontrar menor heuristica
-    {
-        private Punto pto = new Punto();
-        private int heuristica = 0;
-        public Punto Pto { get => pto; set=> pto=value; }
-        public int Heuristica { get =>heuristica; set => heuristica=value; }
-    }
+    
     enum direcciones_t//direcciones guardadas para que despues se impriman
     {
-	UP,DOWN,LEFT,RIGHT
+        UP, DOWN, LEFT, RIGHT
     }
     class Laberinto
     {   //Campos
@@ -26,13 +20,13 @@ namespace Aestrella
         private Punto inicio;
         private Punto final;
         private Punto actual;
-	    private int visitado;//la cantidad total de nodos que visita
-	    private int largo_camino;//es el laargo del camino final
-        private int size;//guarda el tamaño en un numero, si es 10, laberinto es 10x10
-        private List<Punto> visitados=new List<Punto>();//Lista donde se van a guardar puntos que ya hayan sido visitados
-        private List<Punto> caminoCorto = new List<Punto>();//Donde se van a guardar los puntos del camino mas corto
-	    private List<direcciones_t> camino = new List<direcciones_t>();//Se van a guardar donde pasa el camino, agregar cuando visita, eliminar si visita y se devuelve
         
+        private List<Nodo> entrada= new List<Nodo>();//lista que va a guardar nodos que se van a evaluar
+        private List<Nodo> salida = new List<Nodo>();//lista que va a guardar camino final
+        
+        private int size;//guarda el tamaño en un numero, si es 10, laberinto es 10x10
+       
+
         //Metodos
         public Laberinto(String path)//constructor donde se le entrega solo el path del archivo de entrada
         {
@@ -41,11 +35,11 @@ namespace Aestrella
                 StreamReader aux = new StreamReader(path);
 
                 //Lectura de la primera linea manual
-               
+
                 char[] charaux = aux.ReadLine().ToCharArray();//ToCharArray metodo transforma string a arreglo de char
 
 
-                
+
 
                 int size = charaux.Length;//tamaño obtenido manualmente
                 this.size = size;//
@@ -57,13 +51,13 @@ namespace Aestrella
                 char[][] auxlab = new char[size][];
                 auxlab[0] = charaux;
                 int i = 1;//i, contador que se va a usar para rellenar arreglo
-                while (aux.Peek()!=-1) //cuando metodo peek de StreamReader devuelve -1, no hay mas caracteres en el archivo
-                {                       
+                while (aux.Peek() != -1) //cuando metodo peek de StreamReader devuelve -1, no hay mas caracteres en el archivo
+                {
 
                     charaux = aux.ReadLine().ToCharArray();
-                    
-                    
-                  
+
+
+
                     auxlab[i] = charaux;
                     i++;
                 }
@@ -74,31 +68,30 @@ namespace Aestrella
                 //se va a recorrer la matriz entera para definir puntos
                 Punto auxPto1 = new Punto();//inicio
                 Punto auxPto2 = new Punto();//final
-                for (int k=0; k<size; k++)
+                for (int k = 0; k < size; k++)
                 {
                     if (auxlab[k].Contains('A'))
-		    {
-			//Sabiendo que A esta en la fila k, se obtiene la columna mediante metodo que devuelve el indice de arreglo
+                    {
+                        //Sabiendo que A esta en la fila k, se obtiene la columna mediante metodo que devuelve el indice de arreglo
                         auxPto1.i = k;
                         auxPto1.j = Array.FindIndex(auxlab[k], x => x == 'A');
-                        this.inicio= auxPto1;
+                        this.inicio = auxPto1;
                         this.actual = auxPto1;
                     }
                     if (auxlab[k].Contains('B'))
                     {
-			//Sabiendo que B esta en la fila k, se obtiene la columna mediante metodo que devuelve el indice de arreglo
+                        //Sabiendo que B esta en la fila k, se obtiene la columna mediante metodo que devuelve el indice de arreglo
                         auxPto2.i = k;
                         auxPto2.j = Array.FindIndex(auxlab[k], x => x == 'B');
-                        this.final= auxPto2;
+                        this.final = auxPto2;
                     }
                 }
-		//Inicializacion Contadores
-		this.visitado=0;
-		this.largo_camino=0;
-
-
                 
-               
+              
+
+
+
+
 
 
             }
@@ -114,10 +107,10 @@ namespace Aestrella
                 }
                 Console.WriteLine();
             }
-            
+
             Console.WriteLine($"inicio: {inicio.i},{inicio.j}");
             Console.WriteLine($"final: {final.i},{final.j}");
-	    
+
         }
 
         public bool MovValido(Punto punto)
@@ -140,156 +133,29 @@ namespace Aestrella
 
         // Retorna las coordenadas dependiendo de lo que se llame, permitiendo
         // el movimiento hacia las cuatro direcciones.
-        public Punto Arriba(Punto actual) 
+        public Punto Arriba(Punto actual)
         { return new Punto(actual.i - 1, actual.j); }
         public Punto Abajo(Punto actual)
         { return new Punto(actual.i + 1, actual.j); }
         public Punto Izquierda(Punto actual)
-        { return new Punto(actual.i, actual.j - 1);}
-        public Punto Derecha(Punto actual)  
+        { return new Punto(actual.i, actual.j - 1); }
+        public Punto Derecha(Punto actual)
         { return new Punto(actual.i, actual.j + 1); }
 
-        //Metodo que revisa si es posible realizar un mov desde un punto
-        private bool posibleMov(Punto punto)
-        {
-            //revisa cuatro Ptos alrededor si es que se puede ir y si no han sido visitados
-            Punto arriba = Arriba(punto);
-            Punto abajo = Abajo(punto);
-            Punto izquierda = Izquierda(punto);
-            Punto derecha = Derecha(punto);
-            //si por lo menos uno de los cuatro movimientos es posible y no ha sido visitado, se devuelve true
-            if ((MovValido(arriba) && !visitados.Contains(arriba)) ||
-                (MovValido(abajo) && !visitados.Contains(abajo)) ||
-                (MovValido(izquierda) && !visitados.Contains(izquierda)) ||
-                (MovValido(derecha) && !visitados.Contains(derecha))
-                )
-            { return true; }
-            else return false;
-
-        }
-
-        /*Funcion recursiva que evaluará si los alrededores se pueden visitar
-         * y no han sido visitados ya. Lo hará mediante el metodo posibleMov().
-         * En caso de que no se pueda ir a ninguno de los cuatro puntos, 
-         * se volverá al anterior y se hará el mismo procedimiento hasta que llegue a un
-         * punto que tenga alguna posbilidad de movimiento, el cual será devuelto.
-         * 
-         * 
-         * La idea principal de backtrack es tener un metodo que le permita devolverse
-         * Cuando visite puntos que despues no le llevan al camino de menor heuristica
-        */
-        private Punto backtrack(Punto ptoEval, int indiceVisitados)//el inidiceVisitados se puede conseguir con el metodo IndexOf
-        {
 
 
-            if (posibleMov(ptoEval))//si es posible moverse a cualquiera de los cuatro puntos se devuelve el punto
-            {
+       
 
-                return ptoEval;
-
-            }
-            else
-            {
-                visitado++;//Como se va a considerar que se devuelve 1, se le va a sumar a los visitados, este despues se suma al size de la lista visitados
-                largo_camino--;//se le quita al largo camino para despues ajustar su lista y sacar los visitados
-                return backtrack(visitados[indiceVisitados - 1], indiceVisitados - 1);//usando la lista que guardara los visitados se recurre al punto que vino antes.
-            }       
-            }
+       
         //metodo heuristica manhattan normal
-        private int heuristicaManhattan(Punto pto)
+        private void heuristicaManhattan(Nodo nodo) //metodo que calcula el valor de la heuristica del nodo ingresado y se lo asigna
         {
-            return pto.Manhattan(final);
+            nodo.Heuristica = nodo.Pto.Manhattan(this.final);
 
         }
 
-        //metodo de f total con la que va a trabajar algoritmo.
-        private int fTotalManhattan(Punto pto)
-        {
-            int heuristica = heuristicaManhattan(pto);
-            int costoMovimiento = 1;
-            return costoMovimiento + heuristica; 
-        }
-        private Punto menorHeuristica(Punto pto)
-        {
-            //Metodo que compara heuristicas de puntos alrededor al punto entregado, devuelve el punto con menor heuristica.
-
-            //c# es medio enredado con su forma de usar los sort, y me dio lata seguir buscando
-            // asi que use un sort en una lista cualquiera.
-            // si se te ocurre algo mejor dale nomas
-            //recuerda mantener siempre la entrada y la salida de la funcion
-
-            Punto arriba = Arriba(actual);
-            Punto abajo = Abajo(actual);
-            Punto izquierda = Izquierda(actual);
-            Punto derecha = Derecha(actual);
-            List<Par> menorHeuristica = new List<Par>();
-            if (posibleMov(abajo))
-            {
-                Par aux = new Par();
-                aux.Pto = abajo;
-                aux.Heuristica = fTotalManhattan(abajo);
-                menorHeuristica.Add(aux);
-            }
-            if (posibleMov(arriba))
-            {
-                Par aux2 = new Par();
-                aux2.Pto = arriba;
-                aux2.Heuristica = fTotalManhattan(arriba);
-                menorHeuristica.Add(aux2);
-            }
-            if (posibleMov(izquierda))
-            {
-                Par aux3 = new Par();
-                aux3.Pto = izquierda;
-                aux3.Heuristica = fTotalManhattan(izquierda);
-                menorHeuristica.Add(aux3);
-            }
-            if (posibleMov(derecha))
-            {
-                Par aux4 = new Par();
-                aux4.Pto = derecha;
-                aux4.Heuristica = fTotalManhattan(derecha);
-                menorHeuristica.Add(aux4);
-            }
-            
-            menorHeuristica = menorHeuristica.OrderBy(x => x.Heuristica).ToList();
-            return menorHeuristica[0].Pto;
-        }
-        public void AestrellaHManhattan()
-        {
-            largo_camino = 1;//se parte de nodo inicial, costo 1
-            while (actual!=final)
-            { 
-            //Se añade actual a la lista de visitados
-            visitados.Add(actual);
-            //se actualiza el largo del camino corto actual con su tamaño
-            
-            //Se pasa por el metodo backtrack que asegura que va a estar en un lugar que le va a permitir moverse
-            actual = backtrack(actual, visitados.IndexOf(actual));
-
-            //la lista que guardará los puntos del camino mas corto, es decir la que no contara con todos los visitados,
-            //copia la lista de todos los visitados, pero solo hasta la cantidad de la variable largo_camino
-            // la cual se ajusta con el metodo backtrack.
-            //La idea es que se mantiene el largo menor del camino.
-
-            //Mientras mas atras se devuelve se le quita uno al largo del camino final
-            //quedando el largo del camino hasta el punto que si le permite moverse
-
-            //finalmente Con el metodo getrange queda una lista del inicio hasta el lugar 
-            // que le va a permitir moverse despues de haber pasado por el backtrack
-            caminoCorto = visitados.GetRange(0, largo_camino);
-                largo_camino += caminoCorto.Count();
-                //movimiento del punto actual
-                actual = menorHeuristica(actual);
-           
-
-
-
-
-            }
-
-
-
-        }
+        
+       
+        
     }
 }
--- Aestrella/Aestrella/Punto.cs
+++ Aestrella/Aestrella/Punto.cs
@@ -30,4 +30,36 @@ namespace Aestrella
             return Math.Abs(this.i - punto.i) + Math.Abs(this.j - punto.j);
         }
     }
+    internal class Nodo//Clase que se va a usar para guardar los puntos en las listas
+    {
+        private Punto pto;
+        private int heuristica;
+        private int fTotal;
+        private int costoAcumulado;
+        private Nodo? padre;
+        public Punto Pto { get => pto; set => pto = value; }
+        public int Heuristica { get => heuristica; set => heuristica = value; }
+        public int FTotal { get => fTotal; set => fTotal = value; }
+        public int CostoAcumulado { get => costoAcumulado; set => costoAcumulado = value; }
+        internal Nodo Padre { get => padre; set => padre = value; }
+        public Nodo(Nodo padre, Punto pto, int heuristica)
+        {
+            
+            this.Pto = pto;//se ingresa manual
+            this.Heuristica = heuristica;//se calcula en la clase
+            this.Padre = padre;//Se ingresa manual
+            this.FTotal = costoAcumulado+heuristica;
+            this.CostoAcumulado = padre.costoAcumulado+1;//Costo del padre + el costo de movimiento padre a nodo ( en este caso es 1)
+            
+        }
+        public Nodo()//Constructor del primer nodo
+        {   
+
+            this.Pto = new Punto();
+            this.Heuristica = 0;
+            this.FTotal = 0;
+            this.CostoAcumulado = 0;
+            this.Padre = null;
+        }
+    }
 }
